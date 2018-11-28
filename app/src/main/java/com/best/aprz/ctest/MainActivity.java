523b60929f069e1012d8358bb2d6cceab8d8721d
liyunlei <yunleili@sf-express.com>
2018-11-28 21:25:26 +0800

    测试对DataBinding的兼容性

--- app/src/main/java/com/best/aprz/ctest/MainActivity.java
+++ app/src/main/java/com/best/aprz/ctest/MainActivity.java
@@ -2,18 +2,22 @@ package com.best.aprz.ctest;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.best.aprz.ctest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        findViewById(R.id.btn_go_A).setOnClickListener(new View.OnClickListener() {
+        viewDataBinding.btnGoA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        findViewById(R.id.btn_go_B).setOnClickListener(new View.OnClickListener() {
+        viewDataBinding.btnGoB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
